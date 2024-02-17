c3321d3c9fcb1c17ad221e95bb70fee8803bdb0e
Hamza Sufian <hsufiian@yahoo.com>
2024-02-17 20:56:04 +0000

    Add HelloWorld resource to REST API

--- REST_API/main.py
+++ REST_API/main.py
@@ -5,7 +5,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+api.add_resource(HelloWorld, '/helloworld')
 
 
 
