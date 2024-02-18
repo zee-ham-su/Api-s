4479f6bea0800a128258a7d5991faf0c2de36a93
Hamza Sufian <hsufiian@yahoo.com>
2024-02-18 19:20:31 +0000

    Fix import and update response in HelloWorld endpoint

--- REST_API/main.py
+++ REST_API/main.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, requests, jsonify
 from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
 api = Api(app)
+BASE= 'http://127.0.0.1:5000/'
+response = requests.get(BASE + 'helloworld')
+print(response.json())
+
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {"data": 'hello world'}
 
 api.add_resource(HelloWorld, '/helloworld')
 
