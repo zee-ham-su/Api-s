acf1899a48c99a64a8f86a502ce5e9d156494970
Hamza Sufian <hsufiian@yahoo.com>
2024-04-29 14:12:43 +0000

    Add FastAPI endpoint for retrieving lenet model

--- Fastapi/main.py
+++ Fastapi/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+
+class ModelName(str, Enum):
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
 
 
 @app.get("/", description="This is the root path")
@@ -21,5 +27,20 @@ async def get_items():
     return {"message": 'hello from the get items method'}
 
 @app.get("/items/{item_id}")
-async def get_item(item_id: int):
+async def get_item(item_id: bool):
     return {"item_id": item_id}
+
+@app.get('/users/me')
+async def read_users_me():
+    return {'user_id': 'the current user'}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
