87f705353334bbab32c81417ea45627d435c001d
Hamza Sufian <hsufiian@yahoo.com>
2024-04-29 13:00:36 +0000

    Add FastAPI endpoint for retrieving items

--- Fastapi/main.py
+++ Fastapi/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
