e6354e0603cc6ecc44444ca8cff55be7bab3006a
Hamza Sufian <hsufiian@yahoo.com>
2024-05-03 21:27:32 +0000

    feat: Add API endpoints for retrieving and adding products

--- API/index.js
+++ API/index.js
@@ -3,8 +3,38 @@ import  express  from "express";
 
 const app = express();
 
+const products = [
+    {
+        name: 'Laptop',
+        price: 488.96,
+        quantity: 10,
+        active: true
+    },
+    {
+        name: 'Mouse',
+        price: 9.99,
+        quantity: 50,
+        active: true
+    },
+    {
+        name: 'Keyboard',
+        price: 19.99,
+        quantity: 30,
+        active: false
+    }
+];
+
+app.get('/products', (req, res) => {
+    res.status(200).json(products);
+});
+
+app.post('/products', (req, res) => {
+    console.log(req.body);
+    res.send('Adding a product');
+});
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.status(200).send("Hello World");
 });
 
 
