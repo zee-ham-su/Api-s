cfebc4e7b95afeb47ec955b93c70ebb90430c101
Hamza Sufian <hsufiian@yahoo.com>
2024-05-04 20:10:03 +0000

    feat: Add unique ID generation for new products in API endpoint

--- API/index.js
+++ API/index.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-
+import crypto from "crypto";
 
 const app = express();
 
@@ -7,25 +7,34 @@ app.use(express.json());
 
 const products = [
     {
-        name: 'Laptop',
-        price: 488.96,
-        quantity: 10,
-        active: true
+        "id": "1b99b728-7c2c-462c-b23b-fd5a21199983",
+        "name": "Laptop",
+        "price": 488.96,
+        "quantity": 10,
+        "active": true
     },
     {
-        name: 'Mouse',
-        price: 9.99,
-        quantity: 50,
-        active: true
+        "id": "1b99b728-7c2c-462c-b23b-fd5a21199982",
+        "name": "Mouse",
+        "price": 9.99,
+        "quantity": 50,
+        "active": true
     },
     {
-        name: 'Keyboard',
-        price: 19.99,
-        quantity: 30,
-        active: false
+        "id": "1b99b728-7c2c-462c-b23b-fd5a21199981",
+        "name": "Keyboard",
+        "price": 19.99,
+        "quantity": 30,
+        "active": false
+    },
+    {
+        "id": "1b99b728-7c2c-462c-b23b-fd5a21199980",
+        "name": "Power bank",
+        "price": 4500.99,
+        "quantity": 15,
+        "active": true
     }
-];
-
+]
 app.get('/products', (req, res) => {
     res.status(200).json(products);
 });
@@ -33,10 +42,11 @@ app.get('/products', (req, res) => {
 app.post('/products', (req, res) => {
     const { name, price, quantity, active } = req.body;
     if (!name || !price || !quantity || !active) {
-        return res.status(400).send({ message: 'All fields are required' });
+        return res.status(400).json({ message: 'All fields are required' });
      }
-    products.push({ name, price, quantity, active }); 
-    res.status(201).send({ message: 'product successfully added' });
+    const id = crypto.randomUUID()
+    products.push({ id, name, price, quantity, active }); 
+    res.status(201).json({ message: 'product successfully added', id: id });
 });
 
 app.get('/', (req, res) => {
