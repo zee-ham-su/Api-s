80432d99e319b59875d52bc673e2b30d762ed92d
Hamza Sufian <hsufiian@yahoo.com>
2024-05-18 17:29:37 +0000

    chore: Update npm dependency to latest stable version

--- REST_API_AUTH_NODEJS/index.js
+++ REST_API_AUTH_NODEJS/index.js
@@ -1,16 +1,36 @@
 const express = require('express');
+const Datastore = require('nedb-promises');
+const bcrypt = require('bcryptjs');
 
 // initialize express
-
 const app = express();
 
 //configure body-parser to handle post requests
-
 app.use(express.json());
 
+const users = Datastore.create('Users.json');
 
 app.get('/', (req, res) => {
     res.send('REST API Authorization and Authentication');
+});
+
+app.post('/api/auth/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+        return res.status(422).json({ error: "Please fill all fields" });
+        }
+        const hasPassword = await bcrypt.hash(password, 10);
+        const newUser = await users.insert({
+            name,
+            email,
+            password: hasPassword });
+            return res.status(201).json(newUser);
+    }
+    catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
 });
 
 app.listen(3000, () => {
